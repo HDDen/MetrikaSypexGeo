c56910d0e146ec97c1268bf566a2eafe58ea3e59
HDDen <denis@osagohelper.ru>
2022-12-06 12:34:36 +0300

    Added IP history storing

--- example.js
+++ example.js
@@ -8,7 +8,28 @@
     xhr.onload = function(){
         if (window['ym']){
             //console.log('params', xhr.response);
-            ym(metrika_id, 'params', xhr.response);
+            var resp = xhr.response;
+
+            // история ip
+            try{
+                var l = window.localStorage;
+                if (l){
+                    var hist = l.getItem('madm_iphist');
+                    if (hist) hist = JSON.parse(hist);
+                    if (!(hist instanceof Array)){
+                        hist = [hist];
+                    }
+                    resp['ip_history'] = hist;
+                    
+                    // добавим ip в список
+                    hist.push(resp.ip);
+
+                    // сохраним в истории
+                    l.setItem('madm_iphist', JSON.stringify(hist));
+                }
+            } catch(err){}
+
+            ym(metrika_id, 'params', resp);
         }
     }
     xhr.onerror = function(){
@@ -20,4 +41,4 @@
     xhr.send();
 }());
 
-(function(){var metrika_id=XXXXXXXX;var xhr=new XMLHttpRequest;xhr.open("GET","/madmen-includ/SypexGeo/ipinfo.php");xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8");xhr.responseType="json";xhr.onload=function(){if(window["ym"]){ym(metrika_id,"params",xhr.response)}};xhr.onerror=function(){if(window["ym"]){ym(metrika_id,"params",{madmen_ipinfo_blocked:"yes"})}};xhr.send()})();
+!function(){var metrika_id=XXXXXXXX,xhr=new XMLHttpRequest;xhr.open("GET","/madmen-includ/SypexGeo/ipinfo.php"),xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8"),xhr.responseType="json",xhr.onload=function(){if(window.ym){var resp=xhr.response;try{var l=window.localStorage;if(l){var hist=l.getItem("madm_iphist");hist&&(hist=JSON.parse(hist)),hist instanceof Array||(hist=[hist]),resp.ip_history=hist,hist.push(resp.ip),l.setItem("madm_iphist",JSON.stringify(hist))}}catch(err){}ym(metrika_id,"params",resp)}},xhr.onerror=function(){window.ym&&ym(metrika_id,"params",{madmen_ipinfo_blocked:"yes"})},xhr.send()}();
