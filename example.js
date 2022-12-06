715baea20aea2363029e2cb9974cc521f946d01c
HDDen <denis@osagohelper.ru>
2022-12-06 14:16:15 +0300

    Update example.js

--- example.js
+++ example.js
@@ -20,6 +20,8 @@
                         if (!(hist instanceof Array)){
                             hist = [hist];
                         }
+                    } else {
+                        hist = [];
                     }
 
                     // проверяем, есть ли наш ip в истории
@@ -48,4 +50,4 @@
     xhr.send();
 }());
 
-!function(){var metrika_id=XXXXXXXX,xhr=new XMLHttpRequest;xhr.open("GET","/madmen-includ/SypexGeo/ipinfo.php"),xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8"),xhr.responseType="json",xhr.onload=function(){if(window.ym){var resp=xhr.response;try{var l=window.localStorage;if(l){var hist=l.getItem("madm_iphist");hist&&((hist=JSON.parse(hist))instanceof Array||(hist=[hist])),-1===hist.indexOf(resp.ip)&&hist.push(resp.ip),resp.ip_history=hist,l.setItem("madm_iphist",JSON.stringify(hist))}}catch(err){}ym(metrika_id,"params",resp)}},xhr.onerror=function(){window.ym&&ym(metrika_id,"params",{madmen_ipinfo_blocked:"yes"})},xhr.send()}();
+!function(){var metrika_id=XXXXXXXX,xhr=new XMLHttpRequest;xhr.open("GET","/madmen-includ/SypexGeo/ipinfo.php"),xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8"),xhr.responseType="json",xhr.onload=function(){if(window.ym){var resp=xhr.response;try{var l=window.localStorage;if(l){var hist=l.getItem("madm_iphist");hist?(hist=JSON.parse(hist))instanceof Array||(hist=[hist]):hist=[],-1===hist.indexOf(resp.ip)&&hist.push(resp.ip),resp.ip_history=hist,l.setItem("madm_iphist",JSON.stringify(hist))}}catch(err){}ym(metrika_id,"params",resp)}},xhr.onerror=function(){window.ym&&ym(metrika_id,"params",{madmen_ipinfo_blocked:"yes"})},xhr.send()}();
