16beed5c7e273b6e30661836bf78fc5245454c91
HDDen <denis@osagohelper.ru>
2022-12-06 10:52:27 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 (function(){
-    var metrika_id = 55312411;
+    var metrika_id = XXXXXXXX;
     
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/madmen-includ/SypexGeo/ipinfo.php');
@@ -20,4 +20,4 @@
     xhr.send();
 }());
 
-(function(){var metrika_id=55312411;var xhr=new XMLHttpRequest;xhr.open("GET","/madmen-includ/SypexGeo/ipinfo.php");xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8");xhr.responseType="json";xhr.onload=function(){if(window["ym"]){ym(metrika_id,"params",xhr.response)}};xhr.onerror=function(){if(window["ym"]){ym(metrika_id,"params",{madmen_ipinfo_blocked:"yes"})}};xhr.send()})();
+(function(){var metrika_id=XXXXXXXX;var xhr=new XMLHttpRequest;xhr.open("GET","/madmen-includ/SypexGeo/ipinfo.php");xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8");xhr.responseType="json";xhr.onload=function(){if(window["ym"]){ym(metrika_id,"params",xhr.response)}};xhr.onerror=function(){if(window["ym"]){ym(metrika_id,"params",{madmen_ipinfo_blocked:"yes"})}};xhr.send()})();
