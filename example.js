355033c630a0f58993a57b681f1d1d29919985e9
HDDen <denis@osagohelper.ru>
2022-12-06 15:03:45 +0300

    Added Metrika-waiter function and autodetect metrika counter ID

--- example.js
+++ example.js
@@ -1,13 +1,12 @@
 (function(){
-    var metrika_id = XXXXXXXX;
+    var metrika_id = false; // false для автопоиска или конкретный счётчик
     
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/madmen-includ/SypexGeo/ipinfo.php');
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     xhr.responseType = 'json';
     xhr.onload = function(){
-        if (window['ym']){
-            //console.log('params', xhr.response);
+        waitForYm(metrika_id, function(counter, metrika_id){
             var resp = xhr.response;
 
             // история ip
@@ -38,16 +37,37 @@
                 }
             } catch(err){}
 
-            ym(metrika_id, 'params', resp);
-        }
+            console.log('params', resp);
+            if (resp){
+                ym(metrika_id, 'params', resp);
+            } else {
+                console.log("params wasn't sent");
+            }
+        });
     }
     xhr.onerror = function(){
-        //console.log('madmen_ipinfo_blocked');
-        if (window['ym']){
+        waitForYm(metrika_id, function(counter, metrika_id){
+            //console.log('madmen_ipinfo_blocked');
             ym(metrika_id, 'params', {madmen_ipinfo_blocked: 'yes'});
-        }
+        });
     };
     xhr.send();
+
+    /** Ожидание загрузки счетчика Яндекс.Метрики
+     * @param {?(number|string)} ymCounterNum - номер счетчика, если известен
+     * @param {function} callback - получает аргументами объект и номер счетчика
+     * @param {number} interval - интервал проверки готовности счетчика
+     */
+    function waitForYm(ymCounterNum, callback, interval) {
+        if (!callback) return;
+        if (!ymCounterNum) {
+            var metrikaObj  = (window.Ya && (window.Ya.Metrika || window.Ya.Metrika2)) || null;
+            ymCounterNum = (metrikaObj && metrikaObj.counters && (metrikaObj.counters() || [0])[0].id) || 0;
+        }
+        var ymCounterObj = window['yaCounter' + ymCounterNum] || null;
+        if (ymCounterObj) return (callback(ymCounterObj, ymCounterNum), undefined);
+        setTimeout(function () { waitForYm(ymCounterNum, callback, interval); }, interval || 250);
+    }
 }());
 
-!function(){var metrika_id=XXXXXXXX,xhr=new XMLHttpRequest;xhr.open("GET","/madmen-includ/SypexGeo/ipinfo.php"),xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8"),xhr.responseType="json",xhr.onload=function(){if(window.ym){var resp=xhr.response;try{var l=window.localStorage;if(l){var hist=l.getItem("madm_iphist");hist?(hist=JSON.parse(hist))instanceof Array||(hist=[hist]):hist=[],-1===hist.indexOf(resp.ip)&&hist.push(resp.ip),resp.ip_history=hist,l.setItem("madm_iphist",JSON.stringify(hist))}}catch(err){}ym(metrika_id,"params",resp)}},xhr.onerror=function(){window.ym&&ym(metrika_id,"params",{madmen_ipinfo_blocked:"yes"})},xhr.send()}();
+(function(){var metrika_id=false;var xhr=new XMLHttpRequest;xhr.open("GET","/madmen-includ/SypexGeo/ipinfo.php");xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8");xhr.responseType="json";xhr.onload=function(){waitForYm(metrika_id,(function(counter,metrika_id){var resp=xhr.response;try{var l=window.localStorage;if(l){var hist=l.getItem("madm_iphist");if(hist){hist=JSON.parse(hist);if(!(hist instanceof Array)){hist=[hist]}}else{hist=[]}if(hist.indexOf(resp.ip)===-1){hist.push(resp.ip)}resp["ip_history"]=hist;l.setItem("madm_iphist",JSON.stringify(hist))}}catch(err){}console.log("params",resp);if(resp){ym(metrika_id,"params",resp)}else{console.log("params wasn't sent")}}))};xhr.onerror=function(){waitForYm(metrika_id,(function(counter,metrika_id){ym(metrika_id,"params",{madmen_ipinfo_blocked:"yes"})}))};xhr.send();function waitForYm(ymCounterNum,callback,interval){if(!callback)return;if(!ymCounterNum){var metrikaObj=window.Ya&&(window.Ya.Metrika||window.Ya.Metrika2)||null;ymCounterNum=metrikaObj&&metrikaObj.counters&&(metrikaObj.counters()||[0])[0].id||0}var ymCounterObj=window["yaCounter"+ymCounterNum]||null;if(ymCounterObj)return callback(ymCounterObj,ymCounterNum),undefined;setTimeout((function(){waitForYm(ymCounterNum,callback,interval)}),interval||250)}})();
